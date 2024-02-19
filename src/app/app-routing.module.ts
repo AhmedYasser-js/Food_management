893d1f09111dbdcab9781f6db5dcdbd92700d8d8
AhmedYasser-js <ahmedkasso22@gmail.com>
2024-02-19 08:51:33 -0800

    "part in guard"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { AuthGuard } from './Guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
 
 ];
 
--- src/app/auth/components/verify-acount/verify-acount.component.ts
+++ src/app/auth/components/verify-acount/verify-acount.component.ts
@@ -15,20 +15,15 @@ export class VerifyAcountComponent {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private _AuthService: AuthService, private _ToastrService: ToastrService, private _Router: Router, public _MatDialog: MatDialog
   ) { }
-
   message: string = '"Welcome Again"';
-
   verifyCount: FormGroup = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
     code: new FormControl(null, [Validators.required, Validators.minLength(4)])
   })
-
-
   openDialog(): void {
     const dialogRef = this._MatDialog.open(VerifyAcountComponent, {
       data: { name: '' },
     });
-
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed', result);
       if (result) {
@@ -36,7 +31,6 @@ export class VerifyAcountComponent {
       }
     });
   }
-
   onVerifyAcount(data: any) {
     this._AuthService.onVerify(data).subscribe({
       next: (resppone) => {
